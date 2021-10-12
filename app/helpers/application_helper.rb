12023b460fa251634663085d229c2c549c569557
Érick Moreira <erick@sementesrastreadas.com.br>
2021-10-12 14:02:25 -0300

    Adding a base26 helper: - Adding it here to not depend on a url instance and use them on the controller - It mets the requirements for the short_url, but it's far from the necessary on use of a url shortener for real

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def base26
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    loop do
+      random_number = (SecureRandom.rand * 10_000_000).to_i
+      hashed_str = ''
+
+      while random_number.positive?
+        hashed_str = alphabet[random_number % alphabet.length] + hashed_str
+        random_number /= alphabet.length
+      end
+
+      return hashed_str if hashed_str.length == 5
+    end
+  end
 end
